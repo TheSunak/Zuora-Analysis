9ce5100478ae396f983671f534350e303a6db737
Vishal Sunak <vishal@Vishals-MacBook-Pro.local>
2013-11-14 13:07:43 -0500

    Fix file to become modular

--- zuora_mrr.rb
+++ zuora_mrr.rb
@@ -3,97 +3,35 @@
 require 'awesome_print'
 require 'csv'
 
-raw_data = CSV.read('/Users/vishal/code/Zuora-Analysis/test/test_data.csv')
 
-$final_data = []
+class ZuoraMRR
 
+	attr_reader :raw_data, :account_data
 
-def get_rate_plan_effective_start_date (row)
-	[row[7]]
-end
-
-def get_number_of_seats(row)
-	[row[4]]
-end
-
-def get_rate_plan_charge_mrr(row)
-	[row[8]]
-end
-
-def translate_plans_to_mrr(row)
-
-
-end
-
-def compare_dates(current_date, previous_date)
-	if current_date[0] < previous_date[0]
-		["Previous"]
-	else
-		["Current"]
+	def initialize(path)
+		@raw_data = CSV.read(path)
 	end
 
+	def get_data(row)
+		current_row_data = {:account_number => row[1], :rate_plan_start_date => row[7], :seats => row[4], :mrr => row[8], :subscription_date => row[13] }
+		[current_row_data]
+	end
 
-end
-
-def store_results(seats, mrr)
-	final_data << [seats, mrr]
-
-end
-
-
-previous_account_number = ""
-previous_rate_plan_effective_start_date = ""
-previous_rate_plan_charge_mrr = ""
-
-# DON'T FORGET THE SUBSCRIPTION DATE CHANGE WITH YEAR!!!!!!!!!!!!!!!!!!!!!!!
-
-raw_data.each do |row|
-
-
-	current_row_account_number = row[1]
-
-	#Same account
-	if (previous_account_number == current_row_account_number)
-		#p "Current rate plan start date"
-		 current_rate_plan_effective_start_date = get_rate_plan_effective_start_date(row)
-		
-		#p "Current rate plan charge mrr"
-		 current_rate_plan_charge_mrr = get_rate_plan_charge_mrr(row)
-
-		#p "Current Seats"
-		 current_seats = get_number_of_seats(row)
-
-		#p "Data to use:"
-		 data_to_use = compare_dates(current_rate_plan_effective_start_date, previous_rate_plan_effective_start_date)
-
-
-
-		if (data_to_use == "Previous")
-			#p "Previous"
-			p previous_seats,previous_rate_plan_charge_mrr
-		else
-			#p "Current"
-			 p current_seats,current_rate_plan_charge_mrr
-		end
-			
-
-
-	#New Row, just store the data	
-	else
-		#p "Previous Account Number"
-		 previous_account_number = row[1]
-
-		#p "Previous rate plan start date"
-		 previous_rate_plan_effective_start_date = get_rate_plan_effective_start_date(row)
-		
-		#p "Previous rate plan charge mrr"
-		 previous_rate_plan_charge_mrr = get_rate_plan_charge_mrr(row)
-
-		#p "Previous Seats"
-		 previous_seats = get_number_of_seats(row)
+	def process_rows
+		raw_data.each do |row|
+			current_row_data = get_data(row)
+			ap current_row_data 
+		end	
 
 	end
 
+end
+
+if ARGV.size == 1
+	z = ZuoraMRR.new(ARGV.first)
+	z.process_rows
+else
+	puts "Please enter a path"
 end
 
 
