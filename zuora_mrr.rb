508d25d4cef99aa8e043ee49fb6c7a2f95fe6fae
Vishal Sunak <vishal@Vishals-MacBook-Pro.local>
2013-11-14 15:19:43 -0500

    Group the hashes by account

--- zuora_mrr.rb
+++ zuora_mrr.rb
@@ -13,15 +13,22 @@ class ZuoraMRR
 	end
 
 	def get_data(row)
-		current_row_data = {:account_number => row[1], :rate_plan_start_date => row[7], :seats => row[4], :mrr => row[8], :subscription_date => row[13] }
-		[current_row_data]
+		current_row_data = {:account_number => row[1], :rate_plan_start_date => row[7], :seats => row[4], :mrr => row[8], :subscription_date => row[13], :plan => row[11] }
 	end
 
+
+
+
+
 	def process_rows
+		all_data = []
+		
 		raw_data.each do |row|
 			current_row_data = get_data(row)
-			ap current_row_data 
+			all_data << current_row_data 
 		end	
+
+		grouped_accounts = all_data.group_by{|h| h[:account_number]}
 
 	end
 
@@ -29,7 +36,7 @@ end
 
 if ARGV.size == 1
 	z = ZuoraMRR.new(ARGV.first)
-	z.process_rows
+	ap grouped_accounts = z.process_rows
 else
 	puts "Please enter a path"
 end
