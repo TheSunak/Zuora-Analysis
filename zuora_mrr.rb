ff417be13d2bda11a806cd2ebc0ffcd68d2cb2a9
cjlwired <carlos.jose.lazo@gmail.com>
2013-11-15 21:17:48 -0500

    Refactor code for efficiency and with improved style.

--- zuora_mrr.rb
+++ zuora_mrr.rb
@@ -13,46 +13,40 @@ class ZuoraMRR
 	end
 
 	def get_data(row)
-		current_row_data = {:account_number => row[1], :rate_plan_start_date => row[7], :seats => row[4], :mrr => row[8], :subscription_date => row[13], :plan => row[11] }
+		current_row_data = { 
+			:mrr 	 => row[8], 
+			:plan  => row[11],
+			:seats => row[4],
+			:account_number 			=> row[1],
+			:subscription_date 		=> row[13],
+			:rate_plan_start_date => row[7] }
 	end
 
 	def group_by_account_number
-		all_data = []
+		raw_data.collect{|row| get_data(row)}.group_by{|h| h[:account_number]}
+	end
+
+	def determine_oldest_record(grouped_accounts)
+		dates = grouped_accounts.collect do |k,v|
+			v.collect{|value| value[:rate_plan_start_date]}
+		end
 		
-		raw_data.each do |row|
-			current_row_data = get_data(row)
-			all_data << current_row_data 
-		end	
-
-		grouped_accounts = all_data.group_by{|h| h[:account_number]}
-
+		ap dates.max
 	end
-
-	def determine_oldest_record (grouped_accounts)
-			
-			dates = grouped_accounts.collect { |k,v| v.collect{|value| value[:rate_plan_start_date]}}
-
-			ap dates.max
-
-			
-
-
-	
-	end
-
- 
 
 end
+
+################
+# Main Program #
+################
+
+# Example CLI input:
+# 	ruby zuora_mrr.rb <file_path>
 
 if ARGV.size == 1
 	z = ZuoraMRR.new(ARGV.first)
 	grouped_accounts = z.group_by_account_number
 	z.determine_oldest_record(grouped_accounts)
-
 else
-	puts "Please enter a path"
+	puts "Please enter a path."
 end
-
-
-
-
